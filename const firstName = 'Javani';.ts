3c1ed7be204789c1c7a223b1ef8895db0a514356
Javani <moon.blade4@gmail.com>
2024-09-29 13:39:17 +0500

    first

---
+++ const firstName = 'Javani';.ts
@@ -0,0 +1,55 @@
+// Инфа про меня любимого и пиздатого
+// Возраст, внешние данные и т.д.
+const firstName = 'Javani';
+const lastName = 'Aliyev';
+const age = 34;
+const dateOfBirth = '1990/03/26';
+const weight = '194 lbs';
+const height = '5.6 feet';
+const hairColor = 'black';
+const eyesColor = 'brown';
+
+const militaryService = false;//Служба в армии
+
+//Опыт в спорте
+const sportsExperience = 'MMA, Boxing, Taekwondo, Djudo';
+const mmaExpirience = '5 years';
+const boxingExprience = '1 year';
+const taekwondoExperince = '2 years';
+const djudoExperience = '6 months';
+
+//Семейное положение
+const isMarried = true;
+const numberOfChildren = 3;
+const boys = 2;
+const girls = 1;
+
+//Работа и знания
+const placeOfWork = 'Bereke bank';
+const jobTitle = 'manual QA engineer';
+const studyForAvtotests = true;
+const automationLibrary = 'Playwright';
+const programmingLevel = 'Green leaf';
+const purposeOfStudy = 'test automation';
+const energyLevel = 'low';
+const motivationLevel = 'high';
+const stressLevel = 'Ultra super puper high';
+const desireOfSleep = 'Ultra ahuet kak super puper high';
+const workExperience = '18 months';
+const salary = 500000;
+const desiredSalary = 'overdohuya';
+
+//Моя машина и опыт вождения
+const car = 'Huynday Accent';
+const yearOfManufacture = 2014;
+const carColor = 'black';
+const transmission = 'automatic transmtion';
+const mechanicalTransmission = false;
+const machineCondition = 'average';
+const drivingExpirience = '12 years';
+const motorcycleDrivingExpirience = false;
+
+//Хобби
+const bicycleDrivingExperience = true;
+const gamePlayer = true;
+const prefferedGames = 'Gothic, Gothic II, Skyrim, The Witcher 3: Wild hunt'
---
+++ hw.1.2.ts
@@ -0,0 +1,91 @@
+//ДЗ 1.2
+//Задание №1
+const a = 5;
+const multiple = 4 * a;
+console.log (multiple);
+
+//Задание №2
+const s = 6;
+const secondPowerArea = s * s;
+console.log (secondPowerArea);
+
+//Задание №3
+const rectangle_1 = 5;
+const rectangle_2 = 44;
+const multipleRecnatgleSides = rectangle_1 * rectangle_2;
+console.log (multipleRecnatgleSides);
+
+
+// Задание №4
+const d = 53;
+const dDivide = d / 2;
+console.log (dDivide);
+
+
+// Задание №5
+const aA = 33;
+const V = aA*aA*aA;
+const S = aA*aA;
+console.log (V)
+console.log (S);
+
+// Задание №6
+
+const aAa = 50;
+const bBb = 40;
+const cCc = 50;
+const sS = aAa*bBb;
+const vV = sS**3;
+
+console.log (sS)
+console.log (vV)
+
+//Задание №7
+
+const r = 25
+const l = r ** 2;
+const sC = l*3.14;
+console.log (l);
+console.log (sC);
+
+//Задание №8
+const numbA = 50;
+const numbB = 83;
+const numbC = (numbA+numbB);
+const numbD = numbC / 2;
+console.log (numbC);
+console.log (numbD);
+
+
+//Задание №9
+const geoA = 7;
+const geoB = 16;
+const multC = geoA * geoB;
+const sqrt = Math.sqrt(multC);
+console.log (multC);
+console.log (sqrt);
+
+//Задание №10
+const nNullA = 14;
+const nNullB = 11;
+const nNullC = nNullA+nNullB;
+const nNullD = nNullA*nNullB;
+const nNullE = nNullA/nNullB;
+const nNullF = (nNullA**2)/(nNullB **2);
+const nNullG = nNullA - nNullB;
+console.log (nNullA, nNullB, nNullC, nNullD, nNullE, nNullF, nNullG);
+
+
+//Задание №11
+const absA = 18;
+const absB = 14;
+const absC = absA + absB;
+const absD = absA * absB;
+const absE = absA - absB;
+const absF = absA / absB;
+const absG = Math.abs(absA);
+const absH = Math.abs(absB);
+console.log (absC, absD, absE, absF, absG, absH);
+
+
+//Задание №12
